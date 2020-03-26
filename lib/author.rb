dfd0d143efbf28df8cb75720696f93d562baa95c
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-26 22:21:39 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -8,4 +8,13 @@ class Author
 
   def add_post(post)
     post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end
+
+  def self.post_count
+  end
 end
