cb0a2f9d38b2cc761079da1d874fd255147b30ac
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-26 21:14:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,10 +3,16 @@ class Artist
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
   def songs
     Song.all.select{|song| song.artist == self}
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
   end
 
 end
